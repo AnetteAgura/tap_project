462afdf7cc8184ddc2f941ce6d878187ae6bc150
Anette <anetteagura@gmail.com>
2021-04-27 13:00:10 +0300

    Added framework files

---
+++ app/config/constants.php
@@ -0,0 +1,13 @@
+<?php
+// project name
+define('PROJECT_NAME', 'Raamistik');
+// app root dir
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL root
+define('URLROOT', 'http://' . $_SERVER['HTTP_HOST'] . '/' . PROJECT_NAME);
+
+// db params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'raamistik');
---
+++ app/models/Page.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class Page
+{
+
+    /**
+     * Page constructor.
+     */
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+}
