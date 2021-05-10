8dad8924645d812173a74329d14100b36947e9ab
Anette <anetteagura@gmail.com>
2021-05-10 11:38:29 +0300

    Errors added to register form and more...

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -14,9 +14,40 @@ class Users extends Controller
 
     public function register()
     {
-        echo $_SERVER['REQUEST_METHOD'];
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            print_r($_POST);
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            $data = array(
+                'name' => trim($_POST['name']),
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'confirm_password' => trim($_POST['confirm_password']),
+                'name_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+                'confirm_password_err' => ''
+
+            );
+            if (empty($data['name'])) {
+                $data['name_err'] = 'Please enter name';
+            }
+            if (empty($data['email'])) {
+                $data['email_err'] = 'Please enter email';
+            } else if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+                $data['email_err'] = 'Please enter valid email';
+            }
+            if (empty($data['password'])) {
+                $data['password_err'] = 'Please enter password';
+            } else if (strlen($data['password']) < 6) {
+                $data['password_err'] = 'Password must consist at least 6 characters.';
+            }
+            if (empty($data['confirm_password'])) {
+                $data['confirm_password_err'] = 'Please confirm password';
+            } else if (strlen($data['password']) < 6) {
+                $data['confirm_password_err'] = 'Password must consist at least 6 characters.';
+            } else if ($data['password'] !== $data['confirm_password']) {
+                $data['confirm_password_err'] = 'Passwords do not match';
+            }
+            print_r($data);
         } else {
             $this->view('users/register');
         }
--- app/models/User.php
+++ app/models/User.php
@@ -3,5 +3,21 @@
 
 class User
 {
+    private $db;
+
+    /**
+     * User constructor.
+     * @param $db
+     */
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function findUserByEmail($email)
+    {
+        $this->db->query('SELECT * FROM users WHERE email=:email');
+        $this->db->bind('');
+    }
 
 }
