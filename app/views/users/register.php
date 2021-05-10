0dc2713385c9bc0fd42049fd23ed2bed73c5835b
Anette <anetteagura@gmail.com>
2021-05-10 11:01:03 +0300

    Added users register form.

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -1,3 +1,35 @@
 <?php require_once APPROOT . '/views/inc/header.php'; ?>
-<h1>Register User</h1>
+<div class="row">
+    <div class="col-md-6 mx-auto">
+        <!--Register form-->
+        <div class="card card-outline-secondary mt-5">
+            <div class="card-header">
+                <h3 class="mb-0">Sign Up</h3>
+                <p class="mt-2">Please fill the fields below to register!</p>
+            </div>
+            <div class="card-body">
+                <form class="form" role="form">
+                    <div class="form-group">
+                        <lable for="name">Name<sup>*</sup></lable>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Full name">
+                    </div>
+                    <div class="form-group">
+                        <lable for="email">Email<sup>*</sup></lable>
+                        <input type="text" class="form-control" id="email" name="email" placeholder="E-mail">
+                    </div>
+                    <div class="form-group">
+                        <lable for="password">Password<sup>*</sup></lable>
+                        <input type="text" class="form-control" id="password" name="password" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                        <lable for="confirm_password">Password<sup>*</sup></lable>
+                        <input type="text" class="form-control" id="confirm_password" name="confirm_password"
+                               placeholder="Confirm password">
+                    </div>
+                    <div class="btn btn-success btn-lg float-right" type="sumbit">Register</div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 <?php require_once APPROOT . '/views/inc/footer.php'; ?>
